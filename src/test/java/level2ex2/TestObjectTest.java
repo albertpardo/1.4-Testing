4ef44bc1123e3eaae530bf8cfd2fa7bdc542856a
apm <apm@mail.com>
2025-11-17 17:09:51 +0100

    [T4L2E2] refator

--- src/test/java/level2ex2/TestObjectTest.java
+++ src/test/java/level2ex2/TestObjectTest.java
@@ -1,32 +1,32 @@
 package level2ex2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class TestObjectTest {
+    private TestObject testObjectOne;
+    private TestObject testObjectTwo;
+
+    @BeforeEach
+    void createObjects(){
+        testObjectOne = new TestObject("Pepe");
+    }
 
     @Test
     @DisplayName("check that two objects have the same reference")
     void check_that_two_objects_have_the_same_reference(){
-        String nameOne = "Pepe";
-
-        TestObject testObjectOne = new TestObject(nameOne);
-        TestObject testObjectTwo = testObjectOne;
-
-        assertThat(testObjectOne).isSameAs(testObjectTwo);
+       TestObject testObjectTwo = testObjectOne;
+       assertThat(testObjectOne).isSameAs(testObjectTwo);
     }
 
     @Test
     @DisplayName("check that two objects do not have the same reference")
     void check_that_two_objects_do_not_have_the_same_reference(){
-        String nameOne = "Pepe";
-        String nameTwo = "Sol";
-
-        TestObject testObjectOne = new TestObject(nameOne);
-        TestObject testObjectTwo = new TestObject(nameTwo);
-
+        final String NAME_TWO = "Sol";
+        TestObject testObjectTwo = new TestObject(NAME_TWO);
         assertThat(testObjectOne).isNotSameAs(testObjectTwo);
     }
 
