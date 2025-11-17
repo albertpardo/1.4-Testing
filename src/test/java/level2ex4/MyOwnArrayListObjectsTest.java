47af8a05b234449472b0a4a982e980556ee431ad
apm <apm@mail.com>
2025-11-17 14:44:16 +0100

    [T4L2E4] refatored

--- src/test/java/level2ex4/MyOwnArrayListObjectsTest.java
+++ src/test/java/level2ex4/MyOwnArrayListObjectsTest.java
@@ -1,64 +1,53 @@
 package level2ex4;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MyOwnArrayListObjectsTest {
+    private static String STRING_ONE = "StringOne";
+    private static String STRING_TWO = "StringTwo";
+    private static Integer NUMBER = 5;
+
+    private MyOwnArrayListObjects myObjectWithArrayListOfObjects;
+    private MyOwnArrayListObjects myObjectWithArrayListOfObjectsTwo;
+
+    @BeforeEach
+    void createObjects(){
+        myObjectWithArrayListOfObjects = new MyOwnArrayListObjects();
+        myObjectWithArrayListOfObjectsTwo = new MyOwnArrayListObjects();
+
+        myObjectWithArrayListOfObjects.addObject(STRING_ONE);
+        myObjectWithArrayListOfObjects.addObject(NUMBER);
+        myObjectWithArrayListOfObjects.addObject(STRING_TWO);
+
+        myObjectWithArrayListOfObjectsTwo.addObject(STRING_ONE);
+        myObjectWithArrayListOfObjectsTwo.addObject(NUMBER);
+    }
+
     @Test
     @DisplayName("check that one arrayList have objects in the same order that are inserted")
     public void check_that_one_arrayList_have_objects_in_the_same_order_that_are_inserted(){
-        String stringOne = "StringOne";
-        String stringTwo = "StringTwo";
-        Integer number = 5;
-
-        MyOwnArrayListObjects myObjectWithArrayListOfObjects = new MyOwnArrayListObjects();
-        myObjectWithArrayListOfObjects.addObject(stringOne);
-        myObjectWithArrayListOfObjects.addObject(number);
-        myObjectWithArrayListOfObjects.addObject(stringTwo);
-        assertThat(myObjectWithArrayListOfObjects.getArrayList()).containsExactly(stringOne, number, stringTwo);
+        assertThat(myObjectWithArrayListOfObjects.getArrayList()).containsExactly(STRING_ONE, NUMBER, STRING_TWO);
     }
 
     @Test
     @DisplayName("check that one arrayList have objects any order")
     public void check_that_an_arrayList_have_objects_in_any_order(){
-        String stringOne = "StringOne";
-        String stringTwo = "StringTwo";
-        Integer number = 5;
-
-        MyOwnArrayListObjects myObjectWithArrayListOfObjects = new MyOwnArrayListObjects();
-        myObjectWithArrayListOfObjects.addObject(stringOne);
-        myObjectWithArrayListOfObjects.addObject(number);
-        myObjectWithArrayListOfObjects.addObject(stringTwo);
-        assertThat(myObjectWithArrayListOfObjects.getArrayList()).containsExactlyInAnyOrder(number, stringOne, stringTwo);
+        assertThat(myObjectWithArrayListOfObjects.getArrayList()).containsExactlyInAnyOrder(NUMBER, STRING_ONE, STRING_TWO);
     }
 
     @Test
     @DisplayName("check that one arrayList one object appears only once time")
     public void check_that_one_arrayList_one_object_appears_only_once_time(){
-        String stringOne = "StringOne";
-        String stringTwo = "StringTwo";
-        Integer number = 5;
-
-        MyOwnArrayListObjects myObjectWithArrayListOfObjects = new MyOwnArrayListObjects();
-        myObjectWithArrayListOfObjects.addObject(stringOne);
-        myObjectWithArrayListOfObjects.addObject(number);
-        myObjectWithArrayListOfObjects.addObject(stringTwo);
-        assertThat(myObjectWithArrayListOfObjects.getArrayList()).containsOnlyOnce(number);
+        assertThat(myObjectWithArrayListOfObjects.getArrayList()).containsOnlyOnce(NUMBER);
     }
 
     @Test
     @DisplayName("check that one arrayList one object does not appears")
     public void check_that_one_arrayList_one_object_does_not_appears(){
-        String stringOne = "StringOne";
-        String stringTwo = "StringTwo";
-        Integer number = 5;
-
-        MyOwnArrayListObjects myObjectWithArrayListOfObjects = new MyOwnArrayListObjects();
-        myObjectWithArrayListOfObjects.addObject(stringOne);
-        myObjectWithArrayListOfObjects.addObject(number);
-        assertThat(myObjectWithArrayListOfObjects.getArrayList()).doesNotContain(stringTwo);
+        assertThat(myObjectWithArrayListOfObjectsTwo.getArrayList()).doesNotContain(STRING_TWO);
     }
-
 }
