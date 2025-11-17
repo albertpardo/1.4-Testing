05900359ec28f984b37718d388df1470948a9722
apm <apm@mail.com>
2025-11-17 17:04:56 +0100

    [T4L2E1] refator

--- src/test/java/level2ex1/NewIntegerObjectTest.java
+++ src/test/java/level2ex1/NewIntegerObjectTest.java
@@ -1,30 +1,33 @@
 package level2ex1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class NewIntegerObjectTest {
+    private static final int INT1 = 6;
+    private static Integer integer1;
+    private static Integer integer2;
+
+    @BeforeEach
+    void createObject(){
+        integer1 = NewIntegerObject.createIntegerObject(INT1);
+    }
 
     @Test
     @DisplayName("Check two integer are equals")
     void check_to_integer_are_equals() {
-        int integer = 6;
-        Integer integer1 = NewIntegerObject.createIntegerObject(integer);
-        Integer integer2 = NewIntegerObject.createIntegerObject(integer);
-
+        integer2 = NewIntegerObject.createIntegerObject(INT1);
         assertThat(integer1).isEqualTo(integer2);
     }
 
     @Test
     @DisplayName("Check two integer are not equals")
     void check_to_integer_are_not_equals() {
-        int integerOne = 6;
         int integerTwo = 25;
-        Integer integer1 = NewIntegerObject.createIntegerObject(integerOne);
-        Integer integer2 = NewIntegerObject.createIntegerObject(integerTwo);
-
+        integer2 = NewIntegerObject.createIntegerObject(integerTwo);
         assertThat(integer1).isNotEqualTo(integer2);
     }
 }
